558011f970baa97f787b9acbce055a3f4d2aa678
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-12-27 00:22:04 +0530

    Update App.js
    Added new structure to existing elements

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-import Header from "./components/Header";
+import NotificationsContainer from "./components/NotificationsContainer";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header />
-      <div>
+      <NotificationsContainer>
         <div>
-          <span>Mark Webber</span> reacted to your recent post My first
-          tournament today!
-          <div>1m ago</div>
-        </div>
-        <div>
-          <span>Angela Gray </span>followed you
-          <span>5m ago</span>
-        </div>
-        <div>
-          <span>Jacob Thompson</span> has joined your group Chess Club 1 day ago
-        </div>
-        <div>
-          <span>Rizky Hasanuddin</span>
-          sent you a private message
-          <div>5 days ago</div>
           <div>
-            Hello, thanks for setting up the Chess Club. I've been a member for
-            a few weeks now and I'm already having lots of fun and improving my
-            game.
+            <span>Mark Webber</span> reacted to your recent post My first
+            tournament today!
+            <div>1m ago</div>
+          </div>
+          <div>
+            <span>Angela Gray </span>followed you
+            <span>5m ago</span>
+          </div>
+          <div>
+            <span>Jacob Thompson</span> has joined your group Chess Club 1 day
+            ago
+          </div>
+          <div>
+            <span>Rizky Hasanuddin</span>
+            sent you a private message
+            <div>5 days ago</div>
+            <div>
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
+            </div>
+          </div>
+          <div>
+            <span>Kimberly Smith</span> commented on your picture
+            <div>1 week ago</div>
+          </div>
+          <div>
+            <span>Nathan Peterson</span>
+            reacted to your recent post 5 end-game strategies to increase your
+            win rate
+            <div>2 weeks ago</div>
+          </div>
+          <div>
+            <span> Anna Kim</span>
+            left the group Chess Club
+            <div>2 weeks ago</div>
           </div>
         </div>
-        <div>
-          <span>Kimberly Smith</span> commented on your picture
-          <div>1 week ago</div>
-        </div>
-        <div>
-          <span>Nathan Peterson</span>
-          reacted to your recent post 5 end-game strategies to increase your win
-          rate
-          <div>2 weeks ago</div>
-        </div>
-        <div>
-          <span> Anna Kim</span>
-          left the group Chess Club
-          <div>2 weeks ago</div>
-        </div>
-      </div>
+      </NotificationsContainer>
     </div>
   );
 }
