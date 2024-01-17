41cacbfc06e422eecaccf2a07883c32609824a89
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2024-01-17 23:14:01 +0530

    Update App.js
    Shifted most of the content to Conatainer component to make the App.js file lean and simple

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import NotificationsContainer from "./components/NotificationsContainer";
+import logo from "./logo.svg";
+import Container from "./components/Container/Container";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <NotificationsContainer>
-        <div>
-          <div>
-            <span>Mark Webber</span> reacted to your recent post My first
-            tournament today!
-            <div>1m ago</div>
-          </div>
-          <div>
-            <span>Angela Gray </span>followed you
-            <span>5m ago</span>
-          </div>
-          <div>
-            <span>Jacob Thompson</span> has joined your group Chess Club 1 day
-            ago
-          </div>
-          <div>
-            <span>Rizky Hasanuddin</span>
-            sent you a private message
-            <div>5 days ago</div>
-            <div>
-              Hello, thanks for setting up the Chess Club. I've been a member
-              for a few weeks now and I'm already having lots of fun and
-              improving my game.
-            </div>
-          </div>
-          <div>
-            <span>Kimberly Smith</span> commented on your picture
-            <div>1 week ago</div>
-          </div>
-          <div>
-            <span>Nathan Peterson</span>
-            reacted to your recent post 5 end-game strategies to increase your
-            win rate
-            <div>2 weeks ago</div>
-          </div>
-          <div>
-            <span> Anna Kim</span>
-            left the group Chess Club
-            <div>2 weeks ago</div>
-          </div>
-        </div>
-      </NotificationsContainer>
-    </div>
+    <>
+     <Container />
+      
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+
+    </>
   );
 }
 
