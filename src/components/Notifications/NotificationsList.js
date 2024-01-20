74315afc3d45574ffb8fe52bd58ddfce5048a244
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2024-01-20 23:04:35 +0530

    Update NotificationsList.js
    Added all notification data within the Notification Tab component

--- src/components/Notifications/NotificationsList.js
+++ src/components/Notifications/NotificationsList.js
@@ -5,33 +5,68 @@ import markPFP from "../../assets/images/avatar-mark-webber.webp";
 import angelaPFP from "../../assets/images/avatar-angela-gray.webp";
 import jacobPFP from "../../assets/images/avatar-jacob-thompson.webp";
 import rizkyPFP from "../../assets/images/avatar-rizky-hasanuddin.webp";
+import kimberleyPFP from "../../assets/images/avatar-kimberly-smith.webp";
+import nathanPFP from "../../assets/images/avatar-nathan-peterson.webp";
+import annaPFP from "../../assets/images/avatar-anna-kim.webp";
 
 export default function NotificationsList() {
-    return(
-        <>
-            <NotificationTab unread={true} displayPicture={markPFP} name="Mark Webber" action="reacted to your recent post My first
-            tournament today!" time="1m ago" />
+  return (
+    <>
+      <NotificationTab
+        unread
+        displayPicture={markPFP}
+        name="Mark Webber"
+        action="reacted to your recent post"
+        post="My first
+        tournament today!"
+        time="1m ago"
+      />
 
-            <NotificationTab unread="true" displayPicture={angelaPFP} name="Angela Gray" action="followed you." time="5 m ago" />
-            
-            <NotificationTab unread="true" displayPicture={jacobPFP} name="Jacob Thompson" action="has joined your group Chess Club." time="1 day ago" />
-            
-            <NotificationTab displayPicture={rizkyPFP} name="Rizky Hasanuddin" action="sent you a private message." time="5 days ago" attachedMessage="Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
-                I'm already having lots of fun and improving my game." />
+      <NotificationTab
+        unread
+        displayPicture={angelaPFP}
+        name="Angela Gray"
+        action="followed you"
+        time="5m ago"
+      />
 
-                Rizky Hasanuddin sent you a private message
-                5 days ago
-                Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
-                I'm already having lots of fun and improving my game.
+      <NotificationTab
+        unread
+        displayPicture={jacobPFP}
+        name="Jacob Thompson"
+        action="has joined your group Chess Club"
+        time="1 day ago"
+      />
 
-                Kimberly Smith commented on your picture
-                1 week ago
+      <NotificationTab
+        displayPicture={rizkyPFP}
+        name="Rizky Hasanuddin"
+        action="sent you a private message"
+        time="5 days ago"
+        message="Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+                I'm already having lots of fun and improving my game"
+      />
 
-                Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
-                2 weeks ago
+      <NotificationTab
+        displayPicture={kimberleyPFP}
+        name="Kimberly Smith"
+        action="commented on your picture"
+        time="1 week ago"
+      />
 
-                Anna Kim left the group Chess Club
-                2 weeks ago
-        </>
-    )
+      <NotificationTab
+        displayPicture={nathanPFP}
+        name="Nathan Peterson"
+        action="reacted to your recent post 5 end-game strategies to increase your win rate"
+        time="2 weeks ago"
+      />
+
+      <NotificationTab
+        displayPicture={annaPFP}
+        name="Anna Kim"
+        action="left the group Chess Club"
+        time="2 weeks ago"
+      />
+    </>
+  );
 }
