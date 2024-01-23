fb7e28798a89503af56bc6db12f852bc2eff21fd
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2024-01-23 23:07:05 +0530

    Update NotificationTab.js
    Added relevant parameters for posts with pictures. And elements to render them.

--- src/components/Notifications/NotificationTab.js
+++ src/components/Notifications/NotificationTab.js
@@ -5,19 +5,22 @@ export default function NotificationTab({
   time,
   name,
   action,
+  group,
   unread,
   message,
-  post
+  post,
+  picturePost
 }) {
   return (
     <>
       <div className={`${styles["tabContainer"]} ${unread ? styles["new"] : ""}`}>
         <img className={styles['displayPicture']} src={displayPicture}></img>
         <div>
-          <span className={styles["name"]}>{name}</span> {action} <span className={styles["post"]}>{post}</span>
+          <span className={styles["name"]}>{name}</span> {action} <span className={styles["group"]}>{group}</span> <span className={styles["post"]}>{post}</span>
           {(unread) ? <span className={styles['dot']}></span> : ""}
           <div className={styles["time"]}>{time}</div>
           {message ? <div className={styles["message"]}>{message}</div> : ""}
+          {picturePost ? <div className={styles["picturePost"]}>{picturePost}</div> : ""}
         </div>
       </div>
     </>
